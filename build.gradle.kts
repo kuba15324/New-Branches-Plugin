5c351abe578b9c655108ea44a973d4b5a023a54f
jakub.mrozinski <jakubekmrozinski@gmail.com>
2025-04-09 09:59:29 +0200

    version upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.example"
-version = "1.1"
+version = "1.1.1"
 
 repositories {
     mavenCentral()
@@ -32,7 +32,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("232")
-        untilBuild.set("242.*")
+        untilBuild = provider { null }
     }
 
     signPlugin {
